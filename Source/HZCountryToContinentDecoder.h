8b7b38528662a9eb098cc632e9be5c14a1f3aa8e
Hector Zarate <hecktorzr@gmail.com>
2013-05-12 16:58:37 +0200

    Created class to decode ISO Country codes to Continents

---
+++ Source/HZCountryToContinentDecoder.h
@@ -0,0 +1,13 @@
+//
+//  HZCountryToContinentDecoder.h
+//  TranscontinentalExample
+//
+//  Created by Hector Zarate on 5/12/13.
+//  Copyright (c) 2013 Hector Zarate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HZCountryToContinentDecoder : NSObject
+
+@end
---
+++ Source/HZCountryToContinentDecoder.m
@@ -0,0 +1,13 @@
+//
+//  HZCountryToContinentDecoder.m
+//  TranscontinentalExample
+//
+//  Created by Hector Zarate on 5/12/13.
+//  Copyright (c) 2013 Hector Zarate. All rights reserved.
+//
+
+#import "HZCountryToContinentDecoder.h"
+
+@implementation HZCountryToContinentDecoder
+
+@end
