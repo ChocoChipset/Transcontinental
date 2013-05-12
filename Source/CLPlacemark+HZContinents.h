2082469c0d165983a1857e4273c872abcccafe2a
Hector Zarate <hecktorzr@gmail.com>
2013-05-12 15:29:57 +0200

    Created category targeted to include continents in CLPlacemark objects

---
+++ Source/CLPlacemark+HZContinents.h
@@ -0,0 +1,13 @@
+//
+//  CLPlacemark+HZContinents.h
+//  TranscontinentalExample
+//
+//  Created by Hector Zarate on 5/12/13.
+//  Copyright (c) 2013 Hector Zarate. All rights reserved.
+//
+
+#import <CoreLocation/CoreLocation.h>
+
+@interface CLPlacemark (HZContinents)
+
+@end
---
+++ Source/CLPlacemark+HZContinents.m
@@ -0,0 +1,15 @@
+//
+//  CLPlacemark+HZContinents.m
+//  TranscontinentalExample
+//
+//  Created by Hector Zarate on 5/12/13.
+//  Copyright (c) 2013 Hector Zarate. All rights reserved.
+//
+
+#import "CLPlacemark+HZContinents.h"
+
+
+@implementation CLPlacemark (HZContinents)
+
+
+@end
