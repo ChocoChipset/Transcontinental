8ca59f218563cd8ad30938c3944b327a91b3dcc6
Hector Zarate <hecktorzr@gmail.com>
2013-05-16 19:48:49 +0200

    Implented method -mapView:didUpdateUserLocation: and refactored the main view controller of the example

--- TranscontinentalExample/TranscontinentalExample/HZViewController.m
+++ TranscontinentalExample/TranscontinentalExample/HZViewController.m
@@ -7,48 +7,95 @@
 //
 
 #import "HZViewController.h"
+#import "CLPlacemark+HZContinents.h"
+#import <CoreLocation/CoreLocation.h>
 
 @interface HZViewController ()
+
+@property (nonatomic, strong) CLGeocoder *geocoder;
+
+-(void)displayLocationError;
 
 @end
 
 @implementation HZViewController
+
+#pragma mark - Initializer
+
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    
+    if (self)
+    {
+        _geocoder = [[CLGeocoder alloc] init];
+    }
+    
+    return self;
+}
+
+#pragma mark - View's Loading
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
     self.mapView.delegate = self;
-    
+    self.mapView.userTrackingMode = MKUserTrackingModeNone;
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - Errors
+
+-(void)displayLocationError
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UIAlertView *locationErrorAlertView = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                                         message:@"Your location couldn't be determined or there was an error with the Geocoder class"
+                                                                        delegate:nil
+                                                               cancelButtonTitle:@"OK"
+                                                               otherButtonTitles: nil];
+    
+    [locationErrorAlertView show];
 }
 
 
-#pragma mark - MapView Delegates
+#pragma mark - MapView Delegate
 
 
 - (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
 {
-    #warning Continent Mapping to be implemented
-    
     self.userCoordinateLabel.text = [NSString stringWithFormat:@"%g, %g", userLocation.coordinate.latitude, userLocation.coordinate.longitude];
+    
+    #warning Execute Geocoder call only once!!
+    
+    [self.geocoder reverseGeocodeLocation:userLocation.location
+                        completionHandler:
+     ^(NSArray *placemarks, NSError *error) {
+         
+         if (error == nil)
+         {
+             NSString *continentNameForPlacemark = @"Nowhere";  // No placemarks! Nothing! Nowhere is where you live!
+             
+             if ([placemarks count] > 0)
+             {
+                 CLPlacemark *anyPlacemark = [placemarks lastObject];
+                 
+                 continentNameForPlacemark = [anyPlacemark continent];
+             }
+             
+             self.userContinentLabel.text = continentNameForPlacemark;
+         }
+         else
+         {
+             [self displayLocationError];
+         }
+     }];
+    
 }
 
 
 -(void)mapView:(MKMapView *)mapView didFailToLocateUserWithError:(NSError *)error
 {
-    UIAlertView *userLocationErrorAlertView = [[UIAlertView alloc] initWithTitle:@"Error"
-                                                                         message:@"Your location couldn't be determined"
-                                                                        delegate:nil
-                                                               cancelButtonTitle:@"OK"
-                                                               otherButtonTitles: nil];
-    
-    [userLocationErrorAlertView show];
+    [self displayLocationError];
 }
 
 @end
